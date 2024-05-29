297c2942eb4ec1860c721713cd92135a8b68a880
JUNSEOK <kd02109@gmail.com>
2024-05-29 11:20:21 +0900

    feat: metadata

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,20 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
-export default function DashboardLayout({ children }: { children: React.ReactNode }){
-  
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
